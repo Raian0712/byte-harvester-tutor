94c32904ebe1c17b543608070786702763ad7860
Raian0712 <55100222+Raian0712@users.noreply.github.com>
2021-08-05 19:11:58 +0800

    fix: add process.env.PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
